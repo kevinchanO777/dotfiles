c172d9d2d0f80284102769836900bf18f58def84
kevinhmchan <kevinchan714456@gmail.com>
2025-11-01 23:15:00 +0800

    nvim: remove telescope keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,9 +3,9 @@
 -- Add any additional keymaps here
 
 -- Telescope Keymaps
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-P>", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>ag", builtin.live_grep, { desc = "Telescope find files" })
+-- local builtin = require("telescope.builtin")
+-- vim.keymap.set("n", "<C-P>", builtin.find_files, { desc = "Telescope find files" })
+-- vim.keymap.set("n", "<leader>ag", builtin.live_grep, { desc = "Telescope find files" })
 
 -- Exit insert mode by 'jj'
 vim.keymap.set("i", "jj", "<Esc>")
