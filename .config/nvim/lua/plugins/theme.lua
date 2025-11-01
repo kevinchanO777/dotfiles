e625ac6753552f5883699f446385d28385e15eb5
kevinhmchan <kevinchan714456@gmail.com>
2025-11-02 02:16:51 +0800

    nvim: add more themes

---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    opts = { style = "moon" },
+  },
+
+  {
+    "catppuccin/nvim",
+    lazy = false,
+  },
+
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+  },
+
+  {
+    "shaunsingh/nord.nvim",
+    lazy = false,
+  },
+
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+  },
+
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+        },
+      })
+
+      vim.cmd("colorscheme github_dark")
+    end,
+  },
+
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "github_dark_high_contrast",
+  --   },
+  -- },
+}
