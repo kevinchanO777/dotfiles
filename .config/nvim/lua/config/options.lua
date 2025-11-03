d389c947f19bba01f6866e6ebc7d81ea785517e2
Kevin HM Chan <kevinchan714456@gmail.com>
2025-11-03 22:10:30 +0800

    nvim: fix "Find" "Grep"
    Now opening buffer will not change the behavior of "Find Files" and
    "Live Grep"

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.swapfile = false
+
+-- https://www.reddit.com/r/neovim/comments/1ajpdrx/lazyvim_weird_live_grep_root_dir_functionality_in/
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua#L13
+-- LazyVim root dir detection
+-- Set this to fix "Find" "Grep" to respect "root_dir"
+vim.g.root_spec = { "cwd" }
