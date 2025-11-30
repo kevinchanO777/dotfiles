9079246980c1f257d87494ef1c8eecc27fd0efb2
Kevin HM Chan <kevinchan714456@gmail.com>
2025-11-30 17:45:52 +0800

    wezterm: add wezterm configuration

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Guezwhoz"
+-- config.color_scheme = "Github Light"
+config.font = wezterm.font("JetBrains Mono")
+config.window_background_opacity = 0.96
+config.font_size = 16.0
+
+config.window_decorations = "RESIZE"
+
+-- keybind = cmd+s=text:\x1b:w\r
+config.keys = {
+	{
+		key = "s",
+		mods = "CMD",
+		action = wezterm.action.SendString("\x1b:w\r"),
+	},
+}
+
+return config
