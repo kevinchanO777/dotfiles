e83164ff12fd86e505b51f47c38dfc7b30592343
Kevin HM Chan <kevinchan714456@gmail.com>
2025-12-03 16:52:40 +0800

    lazyvim: add Mason border

--- .config/nvim/lua/plugins/mason-nvim.lua
+++ .config/nvim/lua/plugins/mason-nvim.lua
@@ -4,6 +4,8 @@ return {
   {
     "mason-org/mason.nvim",
     opts = {
+      -- Mason install binaries in ~/.local/share/nvim/mason/bin
+      -- And prepend it to PATH when Mason is loaded in Neovim
       ensure_installed = {
         "stylua",
         "shfmt",
@@ -16,6 +18,9 @@ return {
         "gopls",
         "helm-ls",
         "prettier",
+      },
+      ui = {
+        border = "rounded",
       },
     },
   },
