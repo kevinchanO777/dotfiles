81cc1abbfb55fe6238c10853615f7eecd11f2e95
Kevin HM Chan <kevinchan714456@gmail.com>
2025-12-04 22:17:40 +0800

    tmux: remove .tmp files duplication and clear tmp files

--- .config/tmux/vim-edit-tmux-output.sh
+++ .config/tmux/vim-edit-tmux-output.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 # https://www.reddit.com/r/neovim/comments/1pcum3i/replacing_tmuxs_vim_visual_mode_implementation/
-tmpfile=$(mktemp).tmp
+tmpfile=$(mktemp)
 
 tmux capture-pane -peS -32768 >"$tmpfile"
 
 # tmux run-shell cannot recognise ENV $EDITOR
-tmux new-window -n:panecapture "nvim '+luafile $XDG_CONFIG_HOME/tmux/vi-mode.lua' '+ normal G $' $tmpfile"
+tmux new-window -n:panecapture "nvim '+luafile $XDG_CONFIG_HOME/tmux/vi-mode.lua' '+ normal G $' $tmpfile; rm $tmpfile"
