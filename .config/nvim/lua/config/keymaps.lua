7bb49095d28f3bf3be0f1c931e43f786c2f698dd
Kevin HM Chan <kevinchan714456@gmail.com>
2026-05-15 15:00:42 +0800

    nvim: add more useful keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -13,6 +13,18 @@ vim.keymap.set("n", "k", "kzz", { noremap = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Paste over selected text without overwriting the clipboard register
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+--  Keep serach result at center of page
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
 -- Exit insert mode by 'jj'
 vim.keymap.set("i", "jj", "<Esc>")
 
